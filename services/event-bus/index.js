de17ed50834958ebff40cb6e1d82682925d8d335
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-10 17:13:38 +0100

    wip: event-bus service

---
+++ services/event-bus/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.post('/events', (req, res) => {
+	const event = req.body;
+
+	axios.post('http://localhost:4000/events', event);
+	axios.post('http://localhost:4001/events', event);
+	axios.post('http://localhost:4002/events', event);
+
+	res.send({ status: 'OK' });
+});
+
+app.listen(4004, () => {
+	console.log('Listening on 4004');
+});
