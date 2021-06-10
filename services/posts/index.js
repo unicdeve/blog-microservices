f240c3c8255312f229422b5ac037e23308a709e3
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-10 14:00:16 +0100

    feat: create and get posts

--- services/posts/index.js
+++ services/posts/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 const posts = {};
 
