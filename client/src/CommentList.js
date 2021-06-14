a5f3f8a3363e9022d6d3a39321a179ef2b46b745
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-14 21:11:11 +0100

    feat: display comment status on client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const CommentList = ({ comments }) => {
-	const renderedComments = comments.map((comment) => {
-		return <li key={comment.id}>{comment.content}</li>;
+	const renderedComments = comments.map(({ id, content, status }) => {
+		const finalContent =
+			status === 'approved'
+				? content
+				: status === 'pending'
+				? 'This comment is awaiting moderation'
+				: status === 'rejected'
+				? 'This comment was rejected'
+				: null;
+
+		return <li key={id}>{finalContent}</li>;
 	});
 
 	return <ul>{renderedComments}</ul>;
