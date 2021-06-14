157ea98bc2075a50482ad38fe509233f809b9226
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-14 20:27:29 +0100

    feat: comment moderation service

--- services/comments/index.js
+++ services/comments/index.js
@@ -25,6 +25,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 	comments.push({
 		id,
 		content,
+		status: 'pending',
 	});
 
 	commentsByPostId[postId] = comments;
@@ -36,6 +37,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 				id,
 				content,
 				postId,
+				status: 'pending',
 			},
 		})
 		.catch((err) => {
@@ -45,8 +47,27 @@ app.post('/posts/:id/comments', async (req, res) => {
 	res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-	console.log('Received event: ', req.body.type);
+app.post('/events', async (req, res) => {
+	const { type, data } = req.body;
+	console.log('Received event: ', type);
+
+	if (type === 'CommentModerated') {
+		const { postId, id, status, content } = data;
+
+		const comments = commentsByPostId[postId];
+
+		const comment = comments.find((c) => c.id === id);
+
+		comment.status = status;
+
+		await axios.post('http://localhost:4004/events', {
+			type: 'CommentUpdated',
+			data: {
+				...comment,
+				postId,
+			},
+		});
+	}
 
 	res.send({});
 });
--- services/query/index.js
+++ services/query/index.js
@@ -23,9 +23,20 @@ app.post('/events', (req, res) => {
 	}
 
 	if (type === 'CommentCreated') {
-		const { id, content, postId } = data;
+		const { id, content, postId, status } = data;
 
-		posts[postId].comments.push({ id, content });
+		posts[postId].comments.push({ id, content, status });
+	}
+
+	if (type === 'CommentUpdated') {
+		const { id, postId, status, content } = data;
+
+		const post = posts[postId];
+
+		const comment = post.comments.find((c) => c.id === id);
+
+		comment.status = status;
+		comment.content = content;
 	}
 
 	res.send({});
